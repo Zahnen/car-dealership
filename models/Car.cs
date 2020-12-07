4104941b2f004f818a08044646309a92d722724e
Zahnen Garner <zahnen@gmail.com>
2020-12-07 14:34:44 -0800

    first commit
    Co-authored-by: Kevin Kirkley <kevinmkirkley@gmail.com>

---
+++ models/Car.cs
@@ -0,0 +1,21 @@
+namespace Dealership.Models
+{
+  public class Car
+  {
+    private string MakeModel;
+    private int Price;
+    private int Miles;
+
+    public Car(string makeModel, int price, int miles)
+    {
+      MakeModel = makeModel;
+      Price = price;
+      Miles = miles;
+    }
+
+    public bool WorthBuying(int maxPrice)
+    {
+      return (Price <= maxPrice);
+    }
+  }
+}
